856695708bd989ae4cfba68d3366ad5eeed0c953
azeam <dennishagg@gmail.com>
2020-04-07 19:57:33 +0200

    update ioctl calls example code

--- example_ioctl/example_ioctl.py
+++ example_ioctl/example_ioctl.py
@@ -1,7 +1,11 @@
 import v4l2
 import fcntl
 
-# Working example code for direct ioctl calls, should be much faster than all the string splitting, but trickier to work with
+# Working example code for direct ioctl calls, should be much faster than all the subprocesses and string splitting, but trickier to work with
+#
+# Tried implementing this but there's barely any performance difference from what I can tell, nor much difference in code quantity.
+# Startup lag is probably mainly caused by gtk widgets building. Might continue with the string splitting, uglier but easier
+#
 # The Python V4L bindings are outdated, some slight modifications have been made for Python3 compatibility 
 def get_detailed_outputs(vd, pixel_format, width, height):
     qctrl = v4l2.v4l2_frmivalenum()
@@ -31,7 +35,7 @@ def get_outputs(pixel_format):
             if (qctrl.type == v4l2.V4L2_FRMSIZE_TYPE_DISCRETE):
                 get_detailed_outputs(vd, qctrl.pixel_format, qctrl.discrete.width, qctrl.discrete.height)
             else:
-                # TODO: translate, can't test
+                # TODO: translate to Python, can't test because no device with stepsize
                 ''' 
                 for (width=frmsize.stepwise.min_width; width< frmsize.stepwise.max_width; width+=frmsize.stepwise.step_width)
                     for (height=frmsize.stepwise.min_height; height< frmsize.stepwise.max_height; height+=frmsize.stepwise.step_height)
@@ -81,6 +85,13 @@ def read_camera_controls():
             pass
         qctrl.id += 1
     vd.close()
+
+def set_value(controlid):
+    vd = open('/dev/video0', 'rb+', buffering=0)
+    vctrl = v4l2.v4l2_control()
+    vctrl.id = controlid
+    vctrl.value = 0 # testing with 0, should read from widget
+    fcntl.ioctl(vd, v4l2.VIDIOC_S_CTRL, vctrl)
 
 def read_base_capabalities():
     vd = open('/dev/video0', 'rb+', buffering=0)
@@ -134,19 +145,21 @@ def read_base_capabalities():
             print("Step:", qctrl.step)
             print("Default:", qctrl.default_value)
             print("Value:", vctrl.value)
+            set_value(vctrl.id) # test setting value
             '''
-            win.label = Gtk.Label(hexpand = True, vexpand = False)
-            win.label.set_text(str(qctrl.name, encoding))
-            win.label.set_size_request(-1, 35)
-            win.label.set_halign(Gtk.Align.END)
-            adj = Gtk.Adjustment(value = vctrl.value, lower = qctrl.minimum, upper = qctrl.maximum, step_increment = qctrl.step, page_increment = 5, page_size=0)
-            win.scale = Gtk.Scale(orientation=Gtk.Orientation.HORIZONTAL, adjustment=adj)
-            win.scale.set_digits(0)
-            win.scale.set_value_pos(Gtk.PositionType.RIGHT)
-            win.scale.set_size_request(-1, 35)
-            win.scale.connect("value-changed", set_int_value, "dev", "setting")
-            win.intcontrolbox.pack_start(win.scale, False, False, 0)
-            win.intlabelbox.pack_start(win.label, False, False, 0)
+            if qctrl.type == 1: # int
+                win.label = Gtk.Label(hexpand = True, vexpand = False)
+                win.label.set_text(str(qctrl.name, encoding))
+                win.label.set_size_request(-1, 35)
+                win.label.set_halign(Gtk.Align.END)
+                adj = Gtk.Adjustment(value = vctrl.value, lower = qctrl.minimum, upper = qctrl.maximum, step_increment = qctrl.step, page_increment = 5, page_size=0)
+                win.scale = Gtk.Scale(orientation=Gtk.Orientation.HORIZONTAL, adjustment=adj)
+                win.scale.set_digits(0)
+                win.scale.set_value_pos(Gtk.PositionType.RIGHT)
+                win.scale.set_size_request(-1, 35)
+                win.scale.connect("value-changed", set_int_value, card, vctrl.id)
+                win.intcontrolbox.pack_start(win.scale, False, False, 0)
+                win.intlabelbox.pack_start(win.label, False, False, 0)
             '''
             if qctrl.type == 3: # is menu
                 while mctrl.index <= qctrl.maximum:
